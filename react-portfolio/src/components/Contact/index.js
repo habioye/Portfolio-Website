f1a72188d153dbcc3c93692d47f59ae36201072d
habioye <78454514+habioye@users.noreply.github.com>
2024-04-25 15:44:12 -0500

    Untracking the node_modules files

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -24,10 +24,36 @@ const Contact = () => {
                         <AnimatedLetters letterClass={letterClass} strArray={['C','o','n','t','a','c','t',' ','m','e']}
                         idx={15}></AnimatedLetters>
                     </h1>
+                    <p>
+                        Some stuff about me.
+                    </p>
+                    <div className="contact-form">
+                        <form>
+                             <ul>
+                                <li className='half'>
+                                    <input type="text" name="name" placeholder="Name" required />
+
+                                    </li>
+                                    <li className="half">
+                                        <input type="email" name="email" placeholder="Email"/>
+                                    </li>
+                                    <li>
+                                        <input placeholder="Subject" type="text" name="subject" required/>
+                                    </li>
+                                    <li>
+                                        <textarea placeholder="Message" name="message" required></textarea>
+                                    </li>
+                                    <li>
+                                        <input type="submit" className="flat-button" value="SEND"/>
+                                    </li>
+                             </ul>
+                        </form>
+                    </div>
                     </div>
                 </div>
             <Loader type="pacman" />
         </>
+
     )
 }
 export default Contact
