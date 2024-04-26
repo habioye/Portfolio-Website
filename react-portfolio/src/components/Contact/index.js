c3571d78e6d41a66570a5e53cd6e1cddc9e7c846
habioye <78454514+habioye@users.noreply.github.com>
2024-04-25 19:16:23 -0500

    Finished creating the contact page layout

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -1,13 +1,15 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import React from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer,Marker, Popup, useMap } from 'react-leaflet';
 
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['l','o','b','o','d','a','n']
-    const jobArray = ['w','e','b','d','e','v','o','p','e','r']
+    const refForm = useRef()
     
     // useEffect(()=> {
     //     return setTimeout(()=> {
@@ -16,6 +18,42 @@ const Contact = () => {
     //     }, 4000)
 
     // }, [])
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_g874xwm', 'template_inox53j', refForm.current,'yH8YLXCtfvvva8TtG')
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+              alert("{error.text}")
+            },
+          )
+      }
+    // const sendEmail = (e) => {
+    //     e.preventDefault()
+
+    //     emailjs
+    //     .sendForm (
+    //         'service_arqa81b',
+    //         'template_581317',
+    //         refForm.current,
+    //         {
+    //             publicKey: 'yH8YLXCtfvvva8TtG',
+    //         }
+
+    //     )
+    //     .then (() => {
+    //         alert('Message successfully sent!')
+    //         window.location.reload(false)
+    //     },
+    // () => {
+    //     alert('Failed to send the message, please try again')
+    // })
+    // }
     return (
         <>
             <div className="container contact-page">
@@ -27,7 +65,8 @@ const Contact = () => {
                     <p>
                         Some stuff about me.
                     </p>
-                    <div className="contact-form">
+                    <div className="contact-form"
+                    ref={refForm} onSubmit={sendEmail}>
                         <form>
                              <ul>
                                 <li className='half'>
@@ -49,7 +88,25 @@ const Contact = () => {
                              </ul>
                         </form>
                     </div>
+                   
                     </div>
+                <div className="info-map">
+                    Hassan Abioye,
+                    <br/>
+                    Chicago,
+                    <br/>
+                    IL <br/>
+                    USA<br/>
+                    <span>Abioyehassan@gmail.com</span>
+                </div>
+                <div className="map-wrap">
+                <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+                </div>
                 </div>
             <Loader type="pacman" />
         </>
