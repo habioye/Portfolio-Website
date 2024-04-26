cdb9345c26e01922db110eb0d1ead57303a305af
habioye <78454514+habioye@users.noreply.github.com>
2024-04-26 18:21:28 -0500

    style the logo

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/logo-h.png'
 import LogoSubtitle from '../../assets/images/Hassan.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
