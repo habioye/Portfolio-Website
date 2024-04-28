46ced03f4deba15158de8065a9ce7326b1d73414
habioye <78454514+habioye@users.noreply.github.com>
2024-04-27 20:27:21 -0500

    removed commented out code and unused variables

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout'
 import Home from './components/Home'
-import About from './components/About'
+import About from "./components/About/About";
 import Contact from './components/Contact'
 
 function App() {
