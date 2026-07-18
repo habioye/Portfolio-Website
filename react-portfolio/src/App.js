7e02dac14e5cb434a39112af3a88ad6481e45e36
habioye <78454514+habioye@users.noreply.github.com>
2026-07-17 22:18:09 -0500

    Updated path with forward slash '/'

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -11,8 +11,8 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Layout/>}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
 
 
